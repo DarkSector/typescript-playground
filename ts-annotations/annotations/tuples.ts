365c332d20b2874fb4d294c23d65a07d818daaef
Pronoy Chopra <pronoyc@gmail.com>
2020-04-25 23:40:24 -0700

    Updated tuples usage

--- ts-annotations/annotations/tuples.ts
+++ ts-annotations/annotations/tuples.ts
@@ -10,3 +10,13 @@ type Drink = [string, boolean, number];
 // instead of listing it manually
 const pepsi: Drink = ['brown', true, 40];
 const tea: Drink = ['brown', false, 0];
+const sprite: Drink = ['clear', true, 40];
+
+// a tuple doesn't show all the info at a glance
+const carSpecs: [number, number] = [400, 3354];
+
+// an object gives more info
+const carStats = {
+    horsepower: 400,
+    weight: 3354
+};
