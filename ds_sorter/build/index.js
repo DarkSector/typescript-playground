cd6fbdbb6fac385c3be0b4a5f2a8353362ff32f8
Pronoy Chopra <pronoyc@gmail.com>
2020-04-29 07:30:10 -0700

    Gotta use primitive specific typeof instead of typescript type guard

--- ds_sorter/build/index.js
+++ ds_sorter/build/index.js
@@ -4,19 +4,23 @@ var Sorter = /** @class */ (function () {
     }
     Sorter.prototype.sort = function () {
         var length = this.collection.length;
-        // If collection is an array of numbers
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
+                // If collection is an array of numbers
+                if (this.collection instanceof Array) { // this is a type guard
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+                // If collection is a string
+                // String are immutable in JS
+                // Strings have chartCodeAt that is used to compare
+                if (typeof this.collection == "string") { // typeof works only for primitives
                 }
             }
         }
-        // If collection is a string
-        // String are immutable in JS
-        // Strings have chartCodeAt that is used to compare
     };
     return Sorter;
 }());
--- ds_sorter/src/index.ts
+++ ds_sorter/src/index.ts
@@ -5,20 +5,31 @@ class Sorter {
     sort(): void {
         const {length} = this.collection;
 
-        // If collection is an array of numbers
         for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length -i -1; j++) {
-                if (this.collection[j] > this.collection[j+1]) {
-                    const temp = this.collection[j];
-                    this.collection[j] = this.collection[j+1];
-                    this.collection[j+1] = temp;
+            for (let j = 0; j < length - i - 1; j++) {
+
+                // If collection is an array of numbers
+                if (this.collection instanceof Array) { // this is a type guard
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
                 }
+
+
+                // If collection is a string
+                // String are immutable in JS
+                // Strings have chartCodeAt that is used to compare
+                if (typeof this.collection == "string") { // typeof works only for primitives
+
+                }
+
+
             }
         }
 
-        // If collection is a string
-        // String are immutable in JS
-        // Strings have chartCodeAt that is used to compare
+
 
     }
 }
