e4aed3086b01a2482f9dfcde052189df4f375dd7
Pronoy Chopra <pronoyc@gmail.com>
2020-05-03 01:00:54 -0700

    Created an interface to constraint the input collection and then added a new string sorting class

--- ds_sorter/build/index.js
+++ ds_sorter/build/index.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumberCollection_1 = require("./NumberCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
 var numbersCollection = new NumberCollection_1.NumberCollection([10, 15, -5, 0]);
+var charactersCollection = new CharactersCollection_1.CharactersCollection("yaXios");
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+var stringSorter = new Sorter_1.Sorter(charactersCollection);
+stringSorter.sort();
+console.log(stringSorter);
 //# sourceMappingURL=index.js.map
--- ds_sorter/src/index.ts
+++ ds_sorter/src/index.ts
@@ -1,8 +1,16 @@
 import {Sorter} from "./Sorter";
 import {NumberCollection} from "./NumberCollection";
+import {CharactersCollection} from "./CharactersCollection";
 
 const numbersCollection = new NumberCollection([10, 15, -5, 0]);
+
+const charactersCollection = new CharactersCollection("yaXios");
 
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+
+const stringSorter = new Sorter(charactersCollection);
+stringSorter.sort();
+console.log(stringSorter);
