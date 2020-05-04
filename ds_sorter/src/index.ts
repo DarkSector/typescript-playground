e2c10fdf0c28d25556331e939fbefabd7c2cb95d
Pronoy Chopra <pronoyc@gmail.com>
2020-05-03 20:58:48 -0700

    LinkedList call

--- ds_sorter/src/index.ts
+++ ds_sorter/src/index.ts
@@ -1,16 +1,28 @@
 import {Sorter} from "./Sorter";
 import {NumberCollection} from "./NumberCollection";
 import {CharactersCollection} from "./CharactersCollection";
+import {LinkedList} from "./LinkedList";
 
-const numbersCollection = new NumberCollection([10, 15, -5, 0]);
+// const numbersCollection = new NumberCollection([10, 15, -5, 0]);
+//
+// const charactersCollection = new CharactersCollection("yaXios");
+//
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+//
+//
+// const stringSorter = new Sorter(charactersCollection);
+// stringSorter.sort();
+// console.log(stringSorter);
 
-const charactersCollection = new CharactersCollection("yaXios");
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(3);
 
-const sorter = new Sorter(numbersCollection);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(numbersCollection.data);
-
-
-const stringSorter = new Sorter(charactersCollection);
-stringSorter.sort();
-console.log(stringSorter);
+linkedList.print();
