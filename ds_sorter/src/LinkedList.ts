3eb9846105e91e56922195d149e9922165195fb3
Pronoy Chopra <pronoyc@gmail.com>
2020-05-03 20:48:35 -0700

    Linked list implementation

---
+++ ds_sorter/src/LinkedList.ts
@@ -0,0 +1,95 @@
+class LinkedListNode {
+    public next: LinkedListNode | null = null;
+
+    constructor(public data: number) {
+    }
+}
+
+export class LinkedList {
+    // starts off empty
+    head: LinkedListNode | null = null;
+
+    add(data: number): void {
+        const node = new LinkedListNode(data);
+
+        // if there isn't a head
+        // make the new node the head
+        if (!this.head) {
+            this.head = node;
+            return; // still means void is respected
+        }
+
+        // since we're adding stuff at the end
+        let tail = this.head;
+        while (tail.next) {
+            tail = tail.next;
+        }
+
+        tail.next = node;
+
+    }
+
+    get length(): number {
+        if (!this.head) {
+            return 0;
+        }
+
+        let length = 1;
+        let node = this.head;
+
+        while (node.next) {
+            length++;
+            node = node.next;
+        }
+
+        return length;
+    }
+
+    at(index: number): LinkedListNode {
+        if (!this.head) {
+            throw new Error("Index out of bounds");
+        }
+
+        let counter = 0;
+        let node: LinkedListNode | null = this.head;
+
+        while (node) {
+            if (counter === index) {
+                return node;
+            }
+            counter++;
+            node = node.next;
+        }
+        throw new Error('Index out of bound');
+    }
+
+    compare(leftIndex: number, rightIndex: number): boolean {
+        if (!this.head) {
+            throw new Error('List is empty');
+        }
+
+        return this.at(leftIndex).data > this.at(rightIndex).data;
+    }
+
+    swap(leftIndex: number, rightIndex: number): void {
+        const leftNode = this.at(leftIndex);
+        const rightNode = this.at(rightIndex);
+
+        const leftHand = leftNode.data;
+        leftNode.data = rightNode.data;
+        rightNode.data = leftHand;
+    }
+
+    print(): void {
+        if (!this.head) {
+            return;
+        }
+
+        let node: LinkedListNode | null = this.head;
+
+        while (node) {
+            console.log(node.data);
+            node = node.next;
+        }
+    }
+}
